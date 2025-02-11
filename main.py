ed4fccf02f0d6f449dee60cf1230403b520d552f
hamid Bezzout <hamid.bezzout.tech@gmail.com>
2025-02-11 17:58:51 +0100

    add the operations function for numpy arrays

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+def operations(v1,v2,oprs):
+    if oprs =="+":
+        if v1.shape==v2.shape:
+            return v1 + v2
+        else:
+            print(" erreur de taille des matrices ")
+    if oprs == "-":
+        return v1-v2
+    if oprs ==".":
+        return np.dot(v1,v2)
+    
+
+v1 = np.array([[1,2],[2,4]])
+v2 = np.array([1,3])
+print(operations(v1,v2,"-"))
+
+    
